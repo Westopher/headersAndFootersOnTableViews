f46163c2fe7373136e800b571f8beb44aeb266cf
West Kraemer <kraemerwest@gmail.com>
2019-04-02 13:34:49 -0400

    first commit with list and title created

---
+++ headersAndFooters/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  headersAndFooters
+//
+//  Created by West Kraemer on 4/2/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var headerLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.headerLabel.layer.cornerRadius = 20
+        
+    }
+
+
+
+
+}
+
