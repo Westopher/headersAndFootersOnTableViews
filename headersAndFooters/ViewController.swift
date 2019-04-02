63f73f6b4e486add5b4bcf22afd8b29ff1aa382b
West Kraemer <kraemerwest@gmail.com>
2019-04-02 14:00:42 -0400

    add headers and footers and stylize

--- headersAndFooters/ViewController.swift
+++ headersAndFooters/ViewController.swift
@@ -20,15 +20,42 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    
 }
 
 extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = UITextView()
+        header.backgroundColor = .gray
+        header.textColor = .white
+        header.text = "Header Section"
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footer = UITextView()
+        footer.backgroundColor = .lightGray
+        footer.textColor = .white
+        footer.text = "Footer Section"
+        return footer
+    }
+    
     
 }
