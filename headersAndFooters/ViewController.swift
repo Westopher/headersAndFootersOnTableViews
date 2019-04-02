ec5034f2c5f16497229e8578ed22919cf9ee454a
West Kraemer <kraemerwest@gmail.com>
2019-04-02 13:40:37 -0400

    add extensions and style title

--- headersAndFooters/ViewController.swift
+++ headersAndFooters/ViewController.swift
@@ -12,16 +12,23 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var headerLabel: UILabel!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.headerLabel.layer.cornerRadius = 20
-        
     }
-
-
-
-
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
